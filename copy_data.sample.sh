4ecf22c5cb9246ac564ebb9297f5282acbb7e36c
eawapownikov <eawapownikov@gmail.com>
2017-10-07 23:56:39 +0300

    Написать скрипт скачивающий изменения в проект

---
+++ copy_data.sample.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd ~/Projects/
+
+SSH_USER=root
+SSH_PWD="<password>"
+SSH_HOST="bookcreator.medclever.com"
+PATH_SERVER_DB="/var/www/bookcreator/protected/data/database.db"
+PATH_SERVER_IMAGES="/var/www/bookcreator/images"
+
+sshpass -p $SSH_PWD scp root@$SSH_HOST:$PATH_SERVER_DB ./Android/PocketDoctor_android/app/src/main/assets/databases
+sshpass -p $SSH_PWD scp -r root@$SSH_HOST:$PATH_SERVER_IMAGES ./Android/PocketDoctor_android/app/src/main/assets
+sshpass -p $SSH_PWD scp root@$SSH_HOST:$PATH_SERVER_DB ./XCode/pocketdoctor_ios/
+sshpass -p $SSH_PWD scp -r root@$SSH_HOST:$PATH_SERVER_IMAGES ./XCode/pocketdoctor_ios/PocketDoctor
